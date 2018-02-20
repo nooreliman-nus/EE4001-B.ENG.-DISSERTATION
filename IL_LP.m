606f22c073309142fb35240b1b22cb80918694d8
nooreliman <36557624+nooreliman@users.noreply.github.com>
2018-02-20 13:04:03 +0800

    Update IL_LP.m

--- IL_LP.m
+++ IL_LP.m
@@ -1,42 +1,40 @@
 function [ solution ] = IL_LP( interval, daily_price, duration, power, energy )
 %This function aims to schedule non-interruptible loads
 %   Inputs:
-%       interval - how long a time interval is in minutes
-%       daily_price - electricity prices(given in half hour intervals)
-%       power - the power rating of the loads
-%       energy - required energy of loads
+%       t - how long a time interval is in minutes
+%       pr - electricity prices(given in half hour intervals)
+%       pw - the power rating of the loads
+%       e - the energy required
 %   Outputs:
 %       F - objective function value
 %       x - optimal schedule
 
-interval = 10; %time step in minutes
-daily_price = ((30/interval)/1000)*xlsread('ElectricityPrice.xlsx'); %$/kWh in half-hour resolution
-daily_price = repelem(daily_price, 30/interval); %repeats each element 3 times to match 10-min resolution
-N = length(daily_price); %no. of periods in 24 hours
-power = 1;
-energy = 10;
+N = length(pr); %no. of periods in 24 hours, no. of variables
 
-%Objective Function
+%Objective Function - min f'*x
 
-f = daily_price * power; %cost
-
-%Lower and upper bounds
-
-LB = zeros(N,1);
-UB = power * ones(N,1);
+f = t * pr; %cost matrix
 
 %   Equality Constraints
 %       Aeq,beq are the matrix and vector components respectively
 
-Aeq = ones(1,N);
-beq = energy/interval;
+Aeq = ones(1,N); %array of ones corresponding to total load consumption = e
+beq = e/t;
 
-%   Inequality Constraints
+%   Inequality Constraints - A*x <= b
 %       A,b are the matrix and vector components respectively
+%       Include upper and lower bounds for each variable
 
-A = eye(N)
-b = power * ones(N,1);
+A_bound_up = eye(N);
+A_bound_low = -1* bound_up;
 
-[solution] = linprog(f',A,b,Aeq,beq,LB,UB);
+A = [A_bound_up;A_bound_low];
+
+b_bound_up = pw * ones(N,1);
+b_bound_low = zeros(N,1);
+
+b = [b_bound_up;b_bound_low];
+
+[x,fval,exitflag] = linprog(f',A,b,Aeq,beq);
+
 end
-
