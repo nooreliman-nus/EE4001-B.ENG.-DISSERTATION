b3faa5fc3052c99fdd292e72598dd7fa6bc73010
nooreliman <36557624+nooreliman@users.noreply.github.com>
2018-02-28 05:22:19 +0800

    Update TCL_LP.m

--- TCL_LP.m
+++ TCL_LP.m
@@ -1,4 +1,4 @@
-function [ solution ] = TCL_LP(t, pr, pw, c, m, temp_up, temp_0, temp_req, temp_en, d)
+function [solution] = TCL_LP(t, pr, pw, c, m, temp_up, temp_0, temp_req, temp_en, d)
 %This function aims to schedule thermostatically controlled loads
 %   Inputs:
 %       t - how long a time interval is in minutes
@@ -41,23 +41,25 @@ f = t * pr; %cost matrix
 %       C - heat consumption at each time step
     C = zeros(N); %initialise matrix)
     for i = 1:N
-        C(i) = d(i) * c * (temp_req - temp_en(i);
+        C(i) = d(i) * c * (temp_req - temp_en(i));
     end
     
     b = zeros(2 * N, 1);
     for i = 1:N
 %b(i, 0) - lower bound of energy accumulated at each time step
-        b(i, 0) = sum(C(0:i + 1)) + m * c * (temp_up - temp_0);
+        b(i, 1) = sum(C(1:i + 1)) + m * c * (temp_up - temp_0);
 %b(i + N, 0) - upper bound of energy accumulated at each time step
-        b(i + N, 0) = -1 * sum(C(0:i + 1));
+        b(i + N, 1) = -1 * sum(C(1:i + 1));
     end
     
 %b_tmp - upper bound and then lower bound for power value at each time step
     b_tmp = zeros(2 * N, 1);
-    b_tmp(0:N) = pw;
+    b_tmp(1:N) = pw;
 %b - value in inequality A * x <= b
     b = [b;b_tmp];
         
-[x,fval,exitflag] = linprog(f',A,b,Aeq,beq);
+[solution,Final_Cost,exitflag] = linprog(f',A,b);
 
+display(Final_Cost)
+display(solution)
 end
